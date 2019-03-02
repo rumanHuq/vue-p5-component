b61601053d25f06457046265236fac8e93deb60b
Majedul Hoque <rumanbsl@gmail.com>
2019-03-02 11:28:26 +0200

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,14 +2,6 @@
 import Vue from 'vue'
 
 declare module "vue-p5-component"
-
-declare module "*.vue" {
-  // @ts-ignore
-  import Vue from "vue";
-
-  export default Vue;
-}
-
 declare module 'vue/types/vue' {
   interface Vue {
     $myProperty: string
