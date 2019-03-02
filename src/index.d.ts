716e74d8390b3fedbb7d92dc7121f66040f65b43
Majedul Hoque <rumanbsl@gmail.com>
2019-03-02 11:26:52 +0200

    work

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+import Vue from 'vue'
+
+declare module "vue-p5-component"
+declare module 'vue/types/vue' {
+  interface Vue {
+    $myProperty: string
+  }
+}
--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -1,17 +1,7 @@
-// @ts-ignore
-import Vue from 'vue'
-
-declare module "vue-p5-component"
 
 declare module "*.vue" {
   // @ts-ignore
   import Vue from "vue";
-
+  
   export default Vue;
-}
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $myProperty: string
-  }
 }
