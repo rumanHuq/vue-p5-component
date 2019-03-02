e043576e74b11f0fc2fb0037071a3fb8a903a4d8
Majedul Hoque <rumanbsl@gmail.com>
2019-03-02 13:03:24 +0200

    ++

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,12 @@ import Vue from "vue";
 
 declare module "vue-p5-component" {
   export interface Ip5 {
-    setup: (sketch:any)=>any;
+    setup: (sketch:Isetup)=>any;
     draw: (sketch:any)=>any;
   }
+}
+
+interface Isetup {
+  createCanvas: (width: number, height: number)=>any;
+  background: (color: number)=>any
 }
