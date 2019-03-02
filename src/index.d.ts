bf25f98cf2253b2eda298a61e512900c30c17230
Majedul Hoque <rumanbsl@gmail.com>
2019-03-02 15:36:42 +0200

    ++

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,14 +4,22 @@ import Vue from "vue";
 declare module "vue-p5-component" {
   export interface Ip5 {
     setup: (sketch:Isetup)=>any;
-    draw: (sketch:any)=>any;
+    draw?: (sketch:any)=>any;
   }
 }
 
 interface Isetup {
   createCanvas: (width: number, height: number)=>any;
   background: (color: number)=>any;
-  createCapture: (captureDevice: "video")=>({
+  createCapture: (captureDevice: captureDevice)=>{
     size: (width: number, height: number)=>any;
-  })
+    hide: ()=>void;
+  }
 }
+
+interface Idraw {
+  tint:(RED: number, GREEN: number, BLUE: number)=>any;
+  image:(captureDevice: captureDevice)=>any;
+}
+
+type captureDevice= "video";
