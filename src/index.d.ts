3c03eac024ed01e544c4b3104e6ef119675239fc
Majedul Hoque <rumanbsl@gmail.com>
2019-03-02 12:59:48 +0200

    ++

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/prefer-default-export */
 // @ts-ignore
-import Vue from 'vue'
+import Vue from "vue";
 
 declare module "vue-p5-component" {
   export interface Ip5 {
